b89bd00ac05b3a26e4decb0041ee7dfd95d88ee4
Omar Adel <omarxadel21@gmail.com>
2024-03-09 16:33:55 +0200

    server:feat: do not accept extra fields in auth dto validation

--- server/src/dto/auth.dto.ts
+++ server/src/dto/auth.dto.ts
@@ -1,13 +1,17 @@
 import { z } from "zod";
 
-export const ILoginUser = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
+export const ILoginUser = z
+  .object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+  .strict();
 
-export const ISignupUser = z.object({
-  email: z.string().email(),
-  password: z.string(),
-  name: z.string(),
-  phoneNumber: z.string(),
-});
+export const ISignupUser = z
+  .object({
+    email: z.string().email(),
+    password: z.string(),
+    name: z.string(),
+    phoneNumber: z.string(),
+  })
+  .strict();
