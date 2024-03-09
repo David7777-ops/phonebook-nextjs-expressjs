8e3861d937f08b157195fc00a37dfc3fa90a7257
Omar Adel <omarxadel21@gmail.com>
2024-03-09 15:53:19 +0200

    server:feat: added password hashing to signUp feature

--- server/src/services/auth/auth.service.ts
+++ server/src/services/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { z } from "zod";
 import { UsersService } from "../users/users.service";
 import { ISignupUser } from "../../dto/auth.dto";
-
+import bcrypt from "bcrypt";
+import { vars } from "../../config/vars";
 export class AuthService {
   private service = new UsersService();
+
   async signUp(data: z.infer<typeof ISignupUser>) {
+    const password = await bcrypt.hash(data.password, vars.auth.hashRounds);
     const result = await this.service.create({
-      data,
+      data: { ...data, password },
     });
 
     return {
