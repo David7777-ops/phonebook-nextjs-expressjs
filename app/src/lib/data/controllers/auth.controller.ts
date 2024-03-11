b33c175a73a205b03e98eeafae2de50da7e87732
Omar Adel <omarxadel21@gmail.com>
2024-03-11 04:32:57 +0200

    app:fix: fetch throwing error
    refer to: https://github.com/vercel/next.js/discussions/58431

--- app/src/lib/data/controllers/auth.controller.ts
+++ app/src/lib/data/controllers/auth.controller.ts
@@ -6,47 +6,39 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function login({ email, password }: Login) {
-  try {
-    const response = await fetch(API_URL.concat("/auth/login"), {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-      credentials: "include",
-    });
-    const resJson = await response.json();
-    if (resJson.success) {
-      cookies().set("accessToken", resJson.accessToken);
-      cookies().set("refreshToken", resJson.refreshToken);
-      redirect("/");
-    } else {
-      return "Invalid Credentials";
-    }
-  } catch (error) {
-    return "Something went wrong";
+  const response = await fetch(API_URL.concat("/auth/login"), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+    credentials: "include",
+  });
+  const resJson = await response.json();
+  if (resJson.success) {
+    cookies().set("accessToken", resJson.accessToken);
+    cookies().set("refreshToken", resJson.refreshToken);
+    redirect("/");
+  } else {
+    return "Invalid Credentials";
   }
 }
 
 export async function signUp({ email, password, name, phoneNumber }: SignUp) {
-  try {
-    const response = await fetch(API_URL.concat("/auth/signUp"), {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password, name, phoneNumber }),
-    });
+  const response = await fetch(API_URL.concat("/auth/signUp"), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name, phoneNumber }),
+  });
 
-    const resJson = await response.json();
+  const resJson = await response.json();
 
-    if (resJson.success) {
-      return await login({ email, password });
-    } else {
-      if (resJson.status === 409) return "Email already registered";
-      else return "Something went wrong";
-    }
-  } catch (error) {
-    return "Something went wrong";
+  if (resJson.success) {
+    return await login({ email, password });
+  } else {
+    if (resJson.status === 409) return "Email already registered";
+    else return "Something went wrong";
   }
 }
